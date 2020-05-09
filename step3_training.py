4a4f11e0f2a552e99383d803acecd0e567dde510
Tai-Hsien <anigiss@gmail.com>
2020-05-09 01:00:09 -0400

    modify step3.py, add check_gpu in utils.py

--- step3_training.py
+++ step3_training.py
@@ -12,16 +12,16 @@ import pandas as pd
 
 if __name__ == '__main__':
     
-    torch.cuda.set_device(0) # assign which gpu will be used
+    torch.cuda.set_device(utils.get_avail_gpu()) # assign which gpu will be used
     
     train_list = './train_list.csv'
     val_list = './val_list.csv'
     
     num_classes = 3
     num_channels = 1
-    num_epochs = 1
+    num_epochs = 5
     num_workers = 4
-    train_batch_size = 20
+    train_batch_size = 5
     val_batch_size = 1
     num_batches_print = 20
     model_name = 'unet3d_test'
@@ -59,6 +59,37 @@ if __name__ == '__main__':
     
     best_val_dsc = 0.0
     
+    #cudnn
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.enabled = True
+    
+    # eval model first to check gpu memory
+    print('Pre-evaluating model...')
+    unet3d.eval()
+    with torch.no_grad():
+        val_loss_epoch = 0.0
+        val_metric_epoch = 0.0
+        for i_batch, batched_val_sample in enumerate(val_loader):
+            
+            # send mini-batch to device
+            val_inputs, val_labels = batched_val_sample['image'].to(device, dtype=torch.float), batched_val_sample['label'].to(device, dtype=torch.long)
+            one_hot_labels = nn.functional.one_hot(val_labels[:, 0, :, :, :], num_classes=num_classes)
+            one_hot_labels = one_hot_labels.permute(0, 4, 1, 2, 3)
+            
+            val_outputs = unet3d(val_inputs).detach()
+            val_loss = Generalized_Dice_Loss(val_outputs, one_hot_labels, torch.Tensor([0.0, 1.0, 1.0]).to(device, dtype=torch.float).to(device, dtype=torch.float)).detach()
+            val_metric = weighting_DSC(val_outputs, one_hot_labels, torch.Tensor([0.0, 1.0, 1.0]).to(device, dtype=torch.float).to(device, dtype=torch.float)).detach()
+            
+            val_loss_epoch += val_loss.item()
+            val_metric_epoch += val_metric.item()
+            
+        print('[Pre-evaluation, val_loss: {}, val_dsc: {}'.format(val_loss/len(val_loader), val_metric/len(val_loader)))
+        val_loss_epoch = 0.0
+        val_metric_epoch = 0.0
+#        torch.cuda.empty_cache()
+    
+    print('Training model...')
+    class_weights = torch.Tensor([0.05, 1.0, 2.0]).to(device, dtype=torch.float).to(device, dtype=torch.float)
     for epoch in range(num_epochs):
 
         # training
@@ -66,9 +97,7 @@ if __name__ == '__main__':
         running_loss = 0.0
         running_metric = 0.0
         loss_epoch = 0.0
-        metric_epoch = 0.0
-        class_weights = torch.Tensor([0.05, 1.0, 2.0]).to(device, dtype=torch.float).to(device, dtype=torch.float)
-        
+        metric_epoch = 0.0        
         for i_batch, batched_sample in enumerate(train_loader):
 
             # send mini-batch to device            
@@ -81,7 +110,7 @@ if __name__ == '__main__':
             
             # forward + backward + optimize
             outputs = unet3d(inputs)
-            loss = Generalized_Dice_Loss(outputs, one_hot_labels, class_weights) 
+            loss = Generalized_Dice_Loss(outputs, one_hot_labels, class_weights)
             metric = weighting_DSC(outputs, one_hot_labels, class_weights)
             loss.backward()
             opt.step()
@@ -91,8 +120,10 @@ if __name__ == '__main__':
             running_metric += metric.item()
             loss_epoch += loss.item()
             metric_epoch += metric.item()
-            if i_batch % num_batches_print == num_batches_print-1:  # print every 200 mini-batches
+            if i_batch % num_batches_print == num_batches_print-1:  # print every N mini-batches
                 print('[Epoch: {0}/{1}, Batch: {2}/{3}] loss: {4}, dsc: {5}'.format(epoch+1, num_epochs, i_batch+1, len(train_loader), running_loss/num_batches_print, running_metric/num_batches_print))
+                plotter.plot('loss', 'train', 'Loss', epoch+(i_batch+1)/len(train_loader), running_loss/num_batches_print)
+                plotter.plot('DSC', 'train', 'DSC', epoch+(i_batch+1)/len(train_loader), running_metric/num_batches_print)
                 running_loss = 0.0
                 running_metric = 0.0
         
@@ -111,11 +142,13 @@ if __name__ == '__main__':
         #reset
         loss_epoch = 0.0
         metric_epoch = 0.0
-        torch.cuda.empty_cache()
+#        torch.cuda.empty_cache()
                 
         # validation
         unet3d.eval()
         with torch.no_grad():
+            running_val_loss = 0.0
+            running_val_metric = 0.0
             val_loss_epoch = 0.0
             val_metric_epoch = 0.0
             for i_batch, batched_val_sample in enumerate(val_loader):
@@ -125,14 +158,19 @@ if __name__ == '__main__':
                 one_hot_labels = nn.functional.one_hot(val_labels[:, 0, :, :, :], num_classes=num_classes)
                 one_hot_labels = one_hot_labels.permute(0, 4, 1, 2, 3)
                 
-                val_outputs = unet3d(val_inputs)
-                val_loss = Generalized_Dice_Loss(val_outputs, one_hot_labels, class_weights)
-                val_metric = weighting_DSC(val_outputs, one_hot_labels, class_weights)
+                val_outputs = unet3d(val_inputs).detach()
+                val_loss = Generalized_Dice_Loss(val_outputs, one_hot_labels, class_weights).detach()
+                val_metric = weighting_DSC(val_outputs, one_hot_labels, class_weights).detach()
                 
+                running_val_loss += val_loss.item()
+                running_val_metric += val_metric.item()
                 val_loss_epoch += val_loss.item()
                 val_metric_epoch += val_metric.item()
                 
-                print('[Epoch: {0}/{1}, Val batch: {2}/{3}] val_loss: {4}, val_dsc: {5}'.format(epoch+1, num_epochs, i_batch+1, len(val_loader), val_loss, val_metric))
+                if i_batch % num_batches_print == num_batches_print-1:  # print every N mini-batches
+                    print('[Epoch: {0}/{1}, Val batch: {2}/{3}] val_loss: {4}, val_dsc: {5}'.format(epoch+1, num_epochs, i_batch+1, len(val_loader), running_val_loss/num_batches_print, running_val_metric/num_batches_print))
+                    running_val_loss = 0.0
+                    running_val_metric = 0.0
             
             val_losses.append(val_loss_epoch/len(val_loader))
             val_metrics.append(val_metric_epoch/len(val_loader))
@@ -145,12 +183,12 @@ if __name__ == '__main__':
                 torch.save(unet3d.state_dict(), './models/{0}_best.pt'.format(model_name))
             
             # output current status
-            print('*****\n[Epoch: {0}/{1}, loss: {2}, dsc: {3}\n     val_loss: {4}, val_dsc: {5}\n*****'.format(epoch+1, num_epochs, losses[-1], metrics[-1], val_losses[-1], val_metrics[-1]))
+            print('*****\nEpoch: {0}/{1}, loss: {2}, dsc: {3}\n         val_loss: {4}, val_dsc: {5}\n*****'.format(epoch+1, num_epochs, losses[-1], metrics[-1], val_losses[-1], val_metrics[-1]))
             plotter.plot('loss', 'train', 'Loss', epoch+1, losses[-1])
             plotter.plot('DSC', 'train', 'DSC', epoch+1, metrics[-1])
             plotter.plot('loss', 'val', 'Loss', epoch+1, val_losses[-1])
             plotter.plot('DSC', 'val', 'DSC', epoch+1, val_metrics[-1])
-            torch.cuda.empty_cache()
+#            torch.cuda.empty_cache()
             
     # save all losses and metrics data
     pd_dict = {'loss': losses, 'DSC': metrics, 'val_loss': val_losses, 'val_DSC': val_metrics}
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from visdom import Visdom
-
+import socket
 import numpy as np
+import os
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -35,3 +36,33 @@ class VisdomLinePlotter(object):
             ))
         else:
             self.viz.line(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.plots[var_name], name=split_name, update = 'append')
+
+def get_avail_gpu():
+    '''
+    works for linux
+    '''
+    result = os.popen("nvidia-smi").readlines()
+
+    try:
+    # get Processes Line
+        for i in range(len(result)):
+            if 'Processes' in result[i]:
+                process_idx = i
+
+        # get # of gpus
+        num_gpu = 0
+        for i in range(process_idx+1):
+            if 'MiB' in result[i]:
+                num_gpu += 1
+        gpu_list = list(range(num_gpu))
+
+        # dedect which one is busy
+        for i in range(process_idx, len(result)):
+            if result[i][22] == 'C':
+                gpu_list.remove(int(result[i][5]))
+                
+        return (gpu_list[0])
+    except:
+        print('no gpu available, return 0')
+        return 0
+
